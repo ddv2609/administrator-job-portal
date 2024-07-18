84a0f99e7e9f723a1499014744b6bc7b62489db2
ddv2609 <vien26092k2@gmail.com>
2024-07-18 18:33:25 +0700

    Add .gitignore

---
+++ be-job-portal-proj/src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+require('dotenv').config();
+
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const route = require("./routes");
+
+// HTTP request logger middleware for node.js
+app.use(morgan("dev"));
+
+// Middleware for response: x-www-form-urlencoded and json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Middleware for cookie
+app.use(cookieParser());
+
+// Middleware for CORS policy
+app.use(cors({
+  origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+  credentials: true,  
+}));
+
+// Route app
+route(app);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
