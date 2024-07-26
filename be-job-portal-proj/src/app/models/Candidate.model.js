8faf74791cd3ebdd34650a89709dfb7397e66f96
ddv2609 <vien26092k2@gmail.com>
2024-07-27 01:13:44 +0700

    API for candidate

--- be-job-portal-proj/src/app/models/Candidate.model.js
+++ be-job-portal-proj/src/app/models/Candidate.model.js
@@ -6,8 +6,14 @@ const CandidateSchema = new Schema({
   googleId: { type: String },
   facebookId: { type: String },
   linkedinId: { type: String },
-  defaultResume: { type: String },
   education: { type: String },
+  resumes: [{
+    name: { type: String, required: true },
+    resume: { type: String, required: true },
+    viewed: { type: Number, default: 0 },
+    dowloaded: { type: Number, default: 0 },
+    uploadedAt: { type: Date, default: Date.now() }
+  }],
   member: { 
     type: Schema.Types.ObjectId, 
     ref: "Member",
