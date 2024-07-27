4b0f2c8d821fe2c986b103dd398bbcb4cbf9ed34
ddv2609 <vien26092k2@gmail.com>
2024-07-27 08:34:03 +0700

    update API for candidate

--- be-job-portal-proj/src/routes/candidate.route.js
+++ be-job-portal-proj/src/routes/candidate.route.js
@@ -3,10 +3,10 @@ const express = require("express");
 const router = express.Router();
 const candidateController = require("../app/controllers/CandidateController.controller");
 
-router.delete("/:uid/resume/:resumeId", candidateController.deleteResume);
-router.post("/resumes/:uid", candidateController.updateResumes);
-router.post("/avatar/:uid", candidateController.updateAvatar);
-router.post("/info/:uid", candidateController.updateInfo);
-router.get("/info/:uid", candidateController.getInfo);
+router.delete("/resume/:resumeId", candidateController.deleteResume);
+router.post("/resumes/", candidateController.updateResumes);
+router.post("/avatar/", candidateController.updateAvatar);
+router.post("/info/", candidateController.updateInfo);
+router.get("/info/", candidateController.getInfo);
 
 module.exports = router;
