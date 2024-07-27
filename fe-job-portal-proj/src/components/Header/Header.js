246f35f820434638017734a5881b7bcca5ee75c8
Phạm Thanh Tú-B20DCCN610 <thanhtupham1412002@gmail.com>
2024-07-27 15:48:17 +0700

    add home.jsx, edit header and footer

--- fe-job-portal-proj/src/components/Header/Header.js
+++ fe-job-portal-proj/src/components/Header/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
-    navigate("/employer/login");
+    navigate("/login");
   };
   const handleRegisterClick = () => {
     navigate("/employer/sign-up");
@@ -25,98 +25,6 @@ function Header(props) {
           </div>
           <div className={styles.nav}>
             <ul className={styles.navLists}>
-              <li className={styles.menu_dropdonw}>
-                <a href="#job">Việc làm</a>
-                <div className={styles.dropdonw_items_job}>
-                  <ul style={{ textDecoration: "none", listStyleType: "none" }}>
-                    <li>
-                      <a href="#Tifmv= việc làm">
-                        <span class="material-symbols-outlined">search</span>
-                        Tìm việc làm
-                      </a>
-                    </li>
-                    <li>
-                      <a href="# Việc làm đã ứng tuyển">
-                        <span class="material-symbols-outlined">work</span>
-                        Việc làm đã ứng tuyển
-                      </a>
-                    </li>
-                    <li>
-                      <a href="# Việc làm đã lưu ">
-                        <span class="material-symbols-outlined">favorite</span>
-                        Việc làm đã lưu
-                      </a>
-                    </li>
-                    <li>
-                      <a href="# Việc làm phù hợp">
-                        <span class="material-symbols-outlined">
-                          domain_verification
-                        </span>
-                        Việc làm phù hợp
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li className={styles.menu_dropdonw_filecv}>
-                <a href="#resume">Hồ sơ và CV</a>
-                <div className={styles.dropdown_items_filecv}>
-                  <ul style={{ textDecoration: "none", listStyleType: "none" }}>
-                    <li>
-                      <a href="# quản lý cv">
-                        <span class="material-symbols-outlined">
-                          account_box
-                        </span>
-                        Quản lý CV
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#taicvlen">
-                        <span class="material-symbols-outlined">upload</span>
-                        Tải CV lên
-                      </a>
-                    </li>
-                    <li>
-                      <a href="# quản lý cover letter">
-                        <span class="material-symbols-outlined">
-                          description
-                        </span>
-                        Quản lý Cover Letter
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#d">
-                        <span class="material-symbols-outlined">
-                          account_box
-                        </span>
-                        Mẫu CV
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#đ">
-                        <span class="material-symbols-outlined">article</span>
-                        Mẫu Cover Letter
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#d">
-                        <span class="material-symbols-outlined">
-                          support_agent
-                        </span>
-                        Dịch vụ tư vấn CV
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#s">
-                        <span class="material-symbols-outlined">
-                          person_book
-                        </span>
-                        TopCV profile
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
               <li className={styles.menu_dropdonw_company}>
                 <a href="#company">Công ty</a>
                 <div className={styles.dropdown_items_company}>
@@ -141,7 +49,12 @@ function Header(props) {
 
               <li className={styles.menu_dropdonw_hackjob}>
                 <a href="#guide">Cẩm nang nghề nghiệp</a>
-                <div className={styles.dropdonw_items_hackjob}>
+                <div
+                  className={styles.dropdonw_items_hackjob}
+                  style={{
+                    marginLeft: "20px",
+                  }}
+                >
                   <ul style={{ textDecoration: "none", listStyleType: "none" }}>
                     <li>
                       <a href="#danh sách công ty">
