226ebe2366cb67a873d0ba587b3e39ff4c457eb9
ddv2609 <vien26092k2@gmail.com>
2024-08-06 09:20:28 +0700

    API for search jobs

--- be-job-portal-proj/src/app/controllers/JobController.controller.js
+++ be-job-portal-proj/src/app/controllers/JobController.controller.js
@@ -3,11 +3,61 @@ const Job = require("../models/Job.model");
 class JobController {
   // [GET] /api/job/suggestion?page=<number>&size=<number>
   async getAllJobs(req, res) {
-    const { page, size } = req.query;
+    const { page=1, size=0 } = req.query;
 
     try {
       const total = await Job.countDocuments();
       const jobs = await Job.find()
+        .skip((page - 1) * size)
+        .limit(size)
+        .select("-__v -updatedAt -hiddenAt -hiddenBy")
+        .populate("categories")
+        .populate({
+          path: "company",
+          select: "_id name logo companySize field address website",
+        });
+
+      return res.json({
+        jobs,
+        info: {
+          total,
+          page,
+          size,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: error.toString(),
+      });
+    }
+  }
+
+  // [GET] api/job/search?q=<string>&location=<string>&category=<ObjectId>&page=<number>&size=<number>
+  async searchJobs(req, res) {
+    const { page=1, size=0, q="", location, category } = req.query;
+    console.log(q);
+    
+    try {
+      const total = await Job.countDocuments({
+        $or: [
+          { title: { $regex: q, $options: 'i' }, },
+          { locations: {
+            $elemMatch: { province: location }
+          }, },
+          { categories: category }
+        ]
+      });
+
+      const jobs = await Job.find({
+        $or: [
+          { title: { $regex: q, $options: 'i' }, },
+          { locations: {
+            $elemMatch: { province: location }
+          }, },
+          { categories: category }
+        ]
+      })
         .skip((page - 1) * size)
         .limit(size)
         .select("-__v -updatedAt -hiddenAt -hiddenBy")
@@ -40,7 +90,13 @@ class JobController {
       const job = await Job.findOne({
         _id: jobId,
         hidden: false,
-      }).select("-__v -updatedAt -hiddenAt -hiddenBy");
+      })
+        .select("-__v -updatedAt -hiddenAt -hiddenBy")
+        .populate("categories")
+        .populate({
+          path: "company",
+          select: "_id name logo companySize field address website",
+        });
 
       return res.json({
         info: job,
